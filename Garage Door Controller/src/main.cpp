b020215b3556cffec70fd710c1067cb97a3f964c
Erik Biederstadt <biederstadterik@gmail.com>
2024-01-02 13:18:08 -0700

    Basic code for the ESP server

---
+++ Garage Door Controller/src/main.cpp
@@ -0,0 +1,36 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <string>
+
+WebServer server(80);
+
+bool led_on = false;
+int const led_pin = 12;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP("ESP32");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.on("/", []() { server.send(200, "text/plain", "Connected."); });
+  server.on("/blink", []() {
+    led_on = !led_on;
+
+    if (led_on) {
+      Serial.println("LED on");
+      digitalWrite(led_pin, HIGH);
+      server.send(200, "text/plain", "Turned on");
+    } else {
+      Serial.println("LED off");
+      digitalWrite(led_pin, LOW);
+      server.send(200, "text/plain", "Turned off");
+    }
+  });
+
+  server.begin();
+}
+
+void loop() { server.handleClient(); }
