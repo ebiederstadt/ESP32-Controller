760b7f4e8dc52722d1a6ae8907c7758e30935d64
Erik Biederstadt <biederstadterik@gmail.com>
2024-01-05 16:13:49 -0700

    Re-add server files to version control.

---
+++ server/src/main.cpp
@@ -0,0 +1,42 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <fstream>
+#include <string>
+
+#include <password.h>
+
+WebServer server(80);
+
+bool led_on = false;
+int const led_pin = 12;
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+  Serial.begin(115200);
+  WiFi.softAP("ESP32", WIFI_PASSWORD, 1, true);
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+
+  server.on("/", []() { server.send(200, "text/plain", "Connected."); });
+  server.on("/blink", []() {
+    led_on = !led_on;
+
+    if (led_on) {
+      Serial.println("LED on");
+      digitalWrite(led_pin, HIGH);
+      server.send(200, "text/plain", "Turned on");
+    } else {
+      Serial.println("LED off");
+      digitalWrite(led_pin, LOW);
+      server.send(200, "text/plain", "Turned off");
+    }
+  });
+
+  server.begin();
+}
+
+void loop() { server.handleClient(); }
